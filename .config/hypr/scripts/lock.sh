7418f53227b4b9824f01ed48153b3b3899e7e002
Minh <quangminh13032003@gmail.com>
2023-10-17 16:37:26 +0700

    upload

--- .config/hypr/scripts/lock.sh
+++ .config/hypr/scripts/lock.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # If idle for 15s, power down the output
-swaylock --image ~/wallpapers/monochrome/z.png --clock --effect-blur 5x5 --indicator --effect-pixelate 10
+swaylock 
 # Kill the last instance of swayidle so the timer doesn't keep running in background
 pkill --newest swayidle
